47bc3cb6d23b5d7af5af652fff94ca339cd4d97c
passiedaniels <166223164+passiedaniels@users.noreply.github.com>
2025-05-15 19:21:37 +0300

    added the search engine

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 function handleSearchSubmit(event) {
   event.preventDefault();
+  let searchInput = document.querySelector("#search-form");
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
 }
 
 let formTypeElement = document.querySelector("#form-type");
+console.log(formTypeElement);
 formTypeElement.addEventListener("submit", handleSearchSubmit);
