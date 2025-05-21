8980f1b4df64f687bea93e20a5e336eaa4660b76
passiedaniels <166223164+passiedaniels@users.noreply.github.com>
2025-05-21 23:01:01 +0300

    adding api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
+function updateWeatherInfo(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "943a3ddbo20b374aff624c0t29d891a1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form");
 
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let formTypeElement = document.querySelector("#form-type");
 
 formTypeElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("paris");
