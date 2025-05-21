d948c0c5e6fe71d17c884b71cfdaac2056c6d3d7
passiedaniels <166223164+passiedaniels@users.noreply.github.com>
2025-05-22 01:26:00 +0300

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 function updateWeatherInfo(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+
   temperatureElement.innerHTML = Math.round(temperature);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind-speed");
+  let wind = response.data.wind.speed;
+  windElement.innerHTML = Math.round(wind);
+  timeElement.innerHTML = currentDate(date);
+
+  console.log(response.data);
+}
+
+function currentDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes} `;
 }
 
 function searchCity(city) {
